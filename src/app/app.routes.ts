d73b148fe4adb9b81302edfe879bb7e87c4913b9
Luca <luca.podrasa@gmail.com>
2026-06-05 18:47:27 +0200

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { SeasonalCalendar } from './features/seasonal-calendar/seasonal-calendar';
+import { FoodList } from './features/food-list/food-list';
+import { FoodDetails } from './food-details/food-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'seasonal-calendar', component: SeasonalCalendar },
+    { path: 'food-list', component: FoodList },
+    { path: 'food-details/:id', component: FoodDetails }
+];

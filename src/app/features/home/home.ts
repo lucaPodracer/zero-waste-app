1b63715aa6fc63cd8bfee092ea38b3b583c9505d
Jan <janjoker33@googlemail.com>
2026-07-01 17:55:31 +0200

    added seasonal food preview, more foods and made minor Refactorings

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FoodPreviewCard } from './food-preview-card/food-preview-card';
+import { Food } from '../../core/models/food';
+import { FoodService } from '../../core/services/food-service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,27 @@ import { FoodPreviewCard } from './food-preview-card/food-preview-card';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home implements OnInit {
+  seasonalFoods: Food[] = [];
+  previewFoods: Food[] = [];
+
+  constructor(private foodService: FoodService) {}
+
+  ngOnInit(): void {
+    // Anhand des aktuellen Datums saisonale Lebensmittel speichern
+    const currentDate = new Date();
+    this.seasonalFoods = this.foodService.getFoodsBySeason(currentDate.getMonth());
+
+    // Zufällige Lebensmittel für die Vorschau ermitteln und sicherstellen, dass keins doppelt vorkommt
+    const randomIndices = [...Array(this.seasonalFoods.length).keys()]
+      .sort(() => Math.random() - 0.5)
+      .slice(0, 3);   // insgesamt 3 Elemente
+
+    for (const index of randomIndices) {
+      this.previewFoods.push(this.seasonalFoods[index]);
+    }
+  }
+
+  //TODO: Methode für bald ablaufende Lebensmittel aus der Vorratskammer implementieren.
+  //      Dafür wird vermutlich ein "PantryService" oder sowas nötig sein
+}

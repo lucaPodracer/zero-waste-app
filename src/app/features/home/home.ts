cdb8979b0044bc64b0bf971e9cd2c1d5ee611e48
Jan <janjoker33@googlemail.com>
2026-07-02 19:44:30 +0200

    finished expiring pantry items list and implemented responsive design for Home

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, Signal } from '@angular/core';
+import { Component, inject, OnInit, Signal, SimpleChanges } from '@angular/core';
 import { FoodPreviewCard } from './food-preview-card/food-preview-card';
 import { Food } from '../../core/models/food';
 import { FoodService } from '../../core/services/food-service';
@@ -24,6 +24,7 @@ export class Home implements OnInit {
   pantryItems: PantryItem[] = [];
   expiringPantryItems: PantryItem[] = [];
 
+  // Dialog fürs Hinzufügen von Lebensmitteln / Einkäufen
   private pantryDialog = inject(MatDialog);
 
   constructor(
@@ -38,27 +39,27 @@ export class Home implements OnInit {
     const currentDate = new Date();
     this.seasonalFoods = this.foodService.getFoodsBySeason(currentDate.getMonth());
 
-    // Zufällige Lebensmittel für die Vorschau ermitteln und sicherstellen, dass keins doppelt vorkommt
+    // Zufällige saisonale Lebensmittel für die Vorschau ermitteln und sicherstellen, dass keins doppelt vorkommt
     const randomIndices = [...Array(this.seasonalFoods.length).keys()]
       .sort(() => Math.random() - 0.5)
-      .slice(0, 3); // insgesamt 3 Elemente
+      .slice(0, 4); // insgesamt 3 Elemente
 
     for (const index of randomIndices) {
       this.previewFoods.push(this.seasonalFoods[index]);
     }
 
-    // TODO: Den folgenden Teil aus der Init Methode auslagern
-    // Items aus der Vorratsliste laden
+    // FIXME: Die Erbeeren tauchen irgendwie nicht bei den bald ablaufenden Items auf...
+    // Items aus der Vorratsliste laden:
     this.pantryItemsSignal = this.pantryService.getItems();
     this.pantryItems = this.pantryItemsSignal();
 
-    // Alle Lebensmittel filtern, die in 5 Tagen ablaufen
+    // Alle Lebensmittel filtern, die in 5 Tagen oder weniger ablaufen:
     const daysBeforeExpiring = 5;
     const itemWarningDate = new Date();
 
     this.expiringPantryItems = this.pantryItems.filter((item) => {
       if (!item.expiryDate) {
-        //TODO: Sollen Artikel ohne Ablaufdatum berücksichtigt werden oder ignoriert?
+        // Artikel ohne Ablaufdatum werden mit berücksichtigt
         return true;
       }
       itemWarningDate.setDate(item.expiryDate.getDate() - daysBeforeExpiring);

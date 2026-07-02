01d2e6d263442e0076682767f185b8190b32e459
Jan <janjoker33@googlemail.com>
2026-07-02 16:03:04 +0200

    implemented button logic, started work on list of expiring pantry items and made minor changes regarding to responsive design

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,19 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit, Signal } from '@angular/core';
 import { FoodPreviewCard } from './food-preview-card/food-preview-card';
 import { Food } from '../../core/models/food';
 import { FoodService } from '../../core/services/food-service';
+import { PantryItem } from '../../core/models/pantry-item';
+import { PantryService } from '../../core/services/pantry-service';
+import { PantryPreviewCard } from './pantry-preview-card/pantry-preview-card';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { PantryDialog } from '../pantry/components/pantry-dialog/pantry-dialog';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-home',
-  imports: [FoodPreviewCard],
+  imports: [MatButtonModule, MatIconModule, RouterModule, FoodPreviewCard, PantryPreviewCard],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home implements OnInit {
   seasonalFoods: Food[] = [];
   previewFoods: Food[] = [];
+  pantryItemsSignal: Signal<PantryItem[]>;
+  pantryItems: PantryItem[] = [];
+  expiringPantryItems: PantryItem[] = [];
 
-  constructor(private foodService: FoodService) {}
+  private pantryDialog = inject(MatDialog);
+
+  constructor(
+    private foodService: FoodService,
+    private pantryService: PantryService,
+  ) {
+    this.pantryItemsSignal = this.pantryService.getItems();
+  }
 
   ngOnInit(): void {
     // Anhand des aktuellen Datums saisonale Lebensmittel speichern
@@ -23,13 +41,34 @@ export class Home implements OnInit {
     // Zufällige Lebensmittel für die Vorschau ermitteln und sicherstellen, dass keins doppelt vorkommt
     const randomIndices = [...Array(this.seasonalFoods.length).keys()]
       .sort(() => Math.random() - 0.5)
-      .slice(0, 3);   // insgesamt 3 Elemente
+      .slice(0, 3); // insgesamt 3 Elemente
 
     for (const index of randomIndices) {
       this.previewFoods.push(this.seasonalFoods[index]);
     }
+
+    // TODO: Den folgenden Teil aus der Init Methode auslagern
+    // Items aus der Vorratsliste laden
+    this.pantryItemsSignal = this.pantryService.getItems();
+    this.pantryItems = this.pantryItemsSignal();
+
+    // Alle Lebensmittel filtern, die in 5 Tagen ablaufen
+    const daysBeforeExpiring = 5;
+    const itemWarningDate = new Date();
+
+    this.expiringPantryItems = this.pantryItems.filter((item) => {
+      if (!item.expiryDate) {
+        //TODO: Sollen Artikel ohne Ablaufdatum berücksichtigt werden oder ignoriert?
+        return true;
+      }
+      itemWarningDate.setDate(item.expiryDate.getDate() - daysBeforeExpiring);
+      return currentDate.getDate() >= itemWarningDate.getDate();
+    });
   }
 
-  //TODO: Methode für bald ablaufende Lebensmittel aus der Vorratskammer implementieren.
-  //      Dafür wird vermutlich ein "PantryService" oder sowas nötig sein
+  openPantryDialog() {
+    this.pantryDialog.open(PantryDialog, {
+      width: '50%',
+    });
+  }
 }
